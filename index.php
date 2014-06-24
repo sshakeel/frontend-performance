c1075c6808d6f51bfa67e01097a1c154fed8dbb4
Salman Shakeel <sshakeel@xe.com>
2014-06-24 18:19:46 -0400

    trying to json_encode instead

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 	$url = $xml->data->testUrl;
 	$test_date = date("F j, Y, g:i a", strtotime($xml->data->completed));
-	$average = serialize($xml->data->average);
+	$average = json_encode($xml->data->average);
 
 	mysql_query("INSERT INTO webpagetest (testID, url, testdate, average, runs) VALUES('$testID', '$url', '$test_date', '$average', '')");
 
