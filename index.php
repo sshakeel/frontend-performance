4975ce413e5c15ef8bc61bd04e73746e4d17cbd4
Salman Shakeel <sshakeel@xe.com>
2014-06-24 15:39:20 -0400

    adding charts.js for charting data

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="js/chart.js"></script>
+
 	<link rel="stylesheet" href="css/bootstrap.css">
-	<script src="script.js"></script>
+	
 	<title></title>
 </head>
 <?php 
@@ -65,6 +68,41 @@
 					<div class="col-md-2">Tested URL:<br><?php echo $xml->data->testUrl; ?></div>
 				
 				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<canvas id="myChart" width="600" height="400"></canvas>
+				<script>
+					
+					
+
+					var data = {
+						labels : ["January","February","March","April","May","June","July"],
+						datasets : [
+							{
+								fillColor : "rgba(220,220,220,0.5)",
+								strokeColor : "rgba(220,220,220,1)",
+								pointColor : "rgba(220,220,220,1)",
+								pointStrokeColor : "#fff",
+								data : [65,59,90,81,56,55,40]
+							},
+							{
+								fillColor : "rgba(151,187,205,0.5)",
+								strokeColor : "rgba(151,187,205,1)",
+								pointColor : "rgba(151,187,205,1)",
+								pointStrokeColor : "#fff",
+								data : [28,48,40,19,96,27,0]
+							}
+						]
+					}
+
+					//Get the context of the canvas element we want to select
+					var ctx = document.getElementById("myChart").getContext("2d");
+					var myNewChart = new Chart(ctx).Line(data);
+
+
+				</script>
 			</div>
 		</div>
 	</div>
