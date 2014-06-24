4788cf990d06d750c34e434c6259aa6e3a213b39
Salman Shakeel <sshakeel@xe.com>
2014-06-24 19:13:28 -0400

    debugging

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
 	print_r($average_fv);
 
 	$average_fv = json_encode($average_fv);
+	echo $average_fv;
 
 	mysql_query("INSERT INTO webpagetest (testID, url, testdate, average_fv, average_rv, runs) VALUES('$testID', '$url', '$test_date', '$average_fv', '$average_rv', '')");
 
