8e2b5fdbbb9d6866445087ed9c40cfa07f8a70c7
sshakeel <shakeels22@gmail.com>
2014-06-24 23:56:24 -0400

    switching colors

--- index.php
+++ index.php
@@ -63,14 +63,14 @@
 						strokeColor : "rgba(220,220,220,1)",
 						pointColor : "rgba(220,220,220,1)",
 						pointStrokeColor : "#fff",
-						data : [<?php echo implode(',', $avg_load_times_fv); ?>]
+						data : [<?php echo implode(',', $avg_load_times_rv); ?>]
 					},
 					{
 						fillColor : "rgba(151,187,205,0.5)",
 						strokeColor : "rgba(151,187,205,1)",
 						pointColor : "rgba(151,187,205,1)",
 						pointStrokeColor : "#fff",
-						data : [<?php echo implode(',', $avg_load_times_rv); ?>]
+						data : [<?php echo implode(',', $avg_load_times_fv); ?>]
 					}
 				]
 			}
