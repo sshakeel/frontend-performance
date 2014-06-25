75237ec084046ed03f2cbb4cbb2a6d4b09527291
sshakeel <shakeels22@gmail.com>
2014-06-24 21:36:43 -0400

    moving single test stuff into its own page

--- index.php
+++ index.php
@@ -2,86 +2,14 @@
 <?php require_once("functions.php"); ?>
 <?php include("header.php"); ?>
 <?php 
-	$testID = "140621_Z6_51V";
-
-	$xml_url = 'http://www.webpagetest.org/xmlResult/'.$testID.'/';
-	//echo "url: ".$url."<br>";
-
-	$xml = simplexml_load_file($xml_url);
-	//var_dump($xml);
-
 	
-	$url = $xml->data->testUrl;
 	
-	$test_date = date("F j, Y, g:i a", strtotime($xml->data->completed));
-	
-	$average_fv = array('loadTime'=> (string)$xml->data->average->firstView[0]->loadTime, 
-						'TTFB'=> (string)$xml->data->average->firstView[0]->TTFB, 
-						'connections'=> (string)$xml->data->average->firstView[0]->connections, 
-						'requests'=> (string)$xml->data->average->firstView[0]->requests, 
-						'responses_404'=> (string)$xml->data->average->firstView[0]->responses_404, 
-						'render'=> (string)$xml->data->average->firstView[0]->render, 
-						'fullyLoaded'=> (string)$xml->data->average->firstView[0]->fullyLoaded, 
-						'score_cache'=> (string)$xml->data->average->firstView[0]->score_cache, 
-						'score_cdn'=> (string)$xml->data->average->firstView[0]->score_cdn, 
-						'score_gzip'=> (string)$xml->data->average->firstView[0]->score_gzip, 
-						'score_cookies'=> (string)$xml->data->average->firstView[0]->score_cookies, 
-						'score_keep-alive'=> (string)$xml->data->average->firstView[0]->score_keep-alive, 
-						'score_minify'=> (string)$xml->data->average->firstView[0]->score_minify, 
-						'score_compress'=> (string)$xml->data->average->firstView[0]->score_compress);
-	
+	$result = mysql_query("SELECT * FROM webpagetest WHERE '$where'");
+	$row = mysql_fetch_array($result);
+	return $row;
 
-	print_r($average_fv);
-
-	$average_fv = json_encode($average_fv);
-	echo $average_fv;
-
-	mysql_query("INSERT INTO webpagetest (testID, url, testdate, average_fv, average_rv, runs) VALUES('$testID', '$url', '$test_date', '$average_fv', '$average_rv', '')");
-
-	// or...........
-	// foreach ($xml->bbb->cccc as $element) {
-	//   foreach($element as $key => $val) {
-	//    echo "{$key}: {$val}";
-	//   }
-	// }
 ?>
-<div class="row">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title">Connection Info</h3>
-		</div>
-		<div class="panel-body no-padding">
-		
-			<div class="col-md-2 <?php echo ($xml->statusCode == '200' ? 'alert-success' : 'alert-warning'); ?> ">Status:<br><?php echo $xml->statusCode; ?> (<?php echo $xml->statusText; ?>)</div>
-			<div class="col-md-2">Connection:<br><?php echo $xml->data->connectivity; ?></div>
-			<div class="col-md-2">D/L:<br><?php echo ($xml->data->bwDown)/1000; ?> mbps</div>
-			<div class="col-md-2">U/L:<br><?php echo ($xml->data->bwUp)/1000; ?> mbps</div>
-			<div class="col-md-2">Latency:<br><?php echo $xml->data->latency; ?> ms</div>
-			<div class="col-md-2">Completed On:<br><?php echo date("F j, Y, g:i a", strtotime($xml->data->completed));?></div>
-		
-		</div>
-	</div>
-</div>
-<div class="row">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title">Connection Info</h3>
-		</div>
-		<div class="panel-body no-padding">
-		
-			<div class="col-md-1 <?php echo ($xml->statusCode == '200' ? 'alert-success' : 'alert-warning'); ?> ">Status:<br><?php echo $xml->statusCode; ?> (<?php echo $xml->statusText; ?>)</div>
-			<div class="col-md-2">Location:<br><?php echo $xml->data->from; ?></div>
-			<div class="col-md-1">Connection:<br><?php echo $xml->data->connectivity; ?></div>
-			<div class="col-md-1">D/L:<br><?php echo ($xml->data->bwDown)/1000; ?> mbps</div>
-			<div class="col-md-1">U/L:<br><?php echo ($xml->data->bwUp)/1000; ?> mbps</div>
-			<div class="col-md-1">Latency:<br><?php echo $xml->data->latency; ?> ms</div>
-			<div class="col-md-1">Browser:<br><?php echo substr($xml->data->tester, 0, 3); ?></div>
-			<div class="col-md-2">Completed On:<br><?php echo date("F j, Y, g:i a", strtotime($xml->data->completed));?></div>
-			<div class="col-md-2">Tested URL:<br><?php echo $xml->data->testUrl; ?></div>
-		
-		</div>
-	</div>
-</div>
+
 <div class="row">
 	<div class="col-md-12">
 		<canvas id="myChart" width="600" height="400"></canvas>
